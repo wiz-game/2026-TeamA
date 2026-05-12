ea7f729e9976c2b7ff6108c5cf9fa5923c15ab0d
Hikaru Katono <zeal24350106@fsgcl.jp>
2026-05-12 16:45:49 +0900

    Enemyにステートマシン追加

--- GameSources/Enemy.cpp
+++ GameSources/Enemy.cpp
@@ -3,6 +3,48 @@
 
 namespace basecross
 {
+	// Enemyの徘徊中ステート
+	shared_ptr<WonderingState> WonderingState::Instance() 
+	{
+		static shared_ptr<WonderingState> instance(new WonderingState);
+		return instance;
+	}
+	void WonderingState::Enter(const shared_ptr<Enemy>& Obj) 
+	{
+		//
+	}
+	void WonderingState::Execute(const shared_ptr<Enemy>& Obj) 
+	{
+		// 徘徊中の毎フレーム処理
+		Obj->Wondering();
+	}
+	void WonderingState::Exit(const shared_ptr<Enemy>& Obj) 
+	{
+		// wonderingから抜けるときの処理
+
+	}
+
+	// EnemyがPlayerを追跡中ステート
+	shared_ptr<ChasingState> ChasingState::Instance() 
+	{
+		static shared_ptr<ChasingState> instance(new ChasingState);
+		return instance;
+	}
+	void ChasingState::Enter(const shared_ptr<Enemy>& Obj) 
+	{
+		
+	}
+	void ChasingState::Execute(const shared_ptr<Enemy>& Obj) 
+	{
+		
+		Obj->Chasing();
+	}
+	void ChasingState::Exit(const shared_ptr<Enemy>& Obj) 
+	{
+		
+	}
+
+
 
 	void Enemy::OnCreate()
 	{
@@ -13,6 +55,9 @@ namespace basecross
 
 		auto col = AddComponent<CollisionObb>();
 
+		m_stateMachine.reset(new StateMachine<Enemy>(GetThis<Enemy>()));// ステートマシンの初期化
+		m_stateMachine->ChangeState(WonderingState::Instance());
+
 		m_player = GetStage()->GetSharedGameObject<Player>(L"Player");
 
 		damage = 2;
@@ -20,6 +65,8 @@ namespace basecross
 		timer = 0.0f;
 
 		isHit = false;
+
+
 		BrokenObjs::OnCreate();
 		BrokenObjs::SetHP(hp);
 	}
@@ -29,7 +76,7 @@ namespace basecross
 		auto stage = GetStage();
 
 		m_transComp = GetComponent<Transform>();
-		m_transComp->SetPosition(Vec3(-4, 2, 3));
+		m_transComp->SetPosition(Vec3(4, 2, 0));
 		m_transComp->SetScale(Vec3(1, 1, 1));
 		m_transComp->SetRotation(Vec3(0));
 
@@ -46,8 +93,8 @@ namespace basecross
 
 	void Enemy::OnUpdate()
 	{
-		if (!isHit)
-		{
+		//if (!isHit)
+		//{
 			auto& app = App::GetApp();
 			auto scene = app->GetScene<Scene>();
 			float elapsedTime = app->GetElapsedTime();
@@ -63,63 +110,123 @@ namespace basecross
 			}
 
 
-			Vec3 diff = playerPos - enemyPos;
-			float distance = diff.length();
+		//	Vec3 diff = playerPos - enemyPos;
+		//	float distance = diff.length();
 
-			float followRange = 5.0f; //追尾を開始する距離
-			float speed = 1.0f; //速度
-			float followSpeed = 1.5f;//追尾時の速度
+		//	float followRange = 5.0f; //追尾を開始する距離
+		//	float speed = 1.0f; //速度
+		//	float followSpeed = 1.5f;//追尾時の速度
 
-			////追尾していない場合の移動範囲(-10～10)
-			//float rx = -10.0f + (float)rand() / RAND_MAX * 20.0f;
-			//float rz = -10.0f + (float)rand() / RAND_MAX * 20.0f;
+		//	////追尾していない場合の移動範囲(-10～10)
+		//	//float rx = -10.0f + (float)rand() / RAND_MAX * 20.0f;
+		//	//float rz = -10.0f + (float)rand() / RAND_MAX * 20.0f;
 
-			//Vec3 dir = Vec3(rx, 0, rz).normalize();
+		//	//Vec3 dir = Vec3(rx, 0, rz).normalize();
 
-			if (distance < followRange)//一定距離以内
-			{
-				dir = diff.normalize();
+		//	if (distance < followRange)//一定距離以内
+		//	{
+		//		dir = diff.normalize();
 
-				enemyPos += dir * elapsedTime * followSpeed;
-				m_transComp->SetPosition(enemyPos);
+		//		enemyPos += dir * elapsedTime * followSpeed;
+		//		m_transComp->SetPosition(enemyPos);
 
-			}
-			else
-			{
-				timer += elapsedTime;
+		//	}
+		//	else
+		//	{
+		//		timer += elapsedTime;
 
-				if (timer >= 0.0f)
-				{
-					SetValue();
-					dir = Vec3(rx, 0, rz).normalize();
-					timer = -1.0f + (float)rand() / RAND_MAX * -4.0f;
-				}
+		//		if (timer >= 0.0f)
+		//		{
+		//			SetValue();
+		//			dir = Vec3(rx, 0, rz).normalize();
+		//			timer = -1.0f + (float)rand() / RAND_MAX * -4.0f;
+		//		}
 
-				auto ground = GetStage()->GetSharedGameObject<Ground>(L"Ground");
-				Vec3 groundPos = ground->GetComponent<Transform>()->GetPosition();
+		//		auto ground = GetStage()->GetSharedGameObject<Ground>(L"Ground");
+		//		Vec3 groundPos = ground->GetComponent<Transform>()->GetPosition();
 
-				if (enemyPos.x < groundPos.x)
-				{
-					enemyPos += dir * elapsedTime * speed;
-					m_transComp->SetPosition(enemyPos);
-				}
-				else if (enemyPos.z < groundPos.z)
-				{
-					enemyPos += dir * elapsedTime * speed;
-					m_transComp->SetPosition(enemyPos);
-				}
-			}
+		//		if (enemyPos.x < groundPos.x)
+		//		{
+		//			enemyPos += dir * elapsedTime * speed;
+		//			m_transComp->SetPosition(enemyPos);
+		//		}
+		//		else if (enemyPos.z < groundPos.z)
+		//		{
+		//			enemyPos += dir * elapsedTime * speed;
+		//			m_transComp->SetPosition(enemyPos);
+		//		}
+		//	}
 
 
-			wss << L"EnemyPos.x:" << enemyPos.x
-				<< L"\nEnemyPos.y:" << enemyPos.y
-				<< L"\nEnemyPos.z" << enemyPos.z
-				<< L"\nEnemyLife" << BrokenObjs::GetHP() << endl;
-			scene->SetDebugString(wss.str());
+			//wss << L"EnemyPos.x:" << enemyPos.x
+			//	<< L"\nEnemyPos.y:" << enemyPos.y
+			//	<< L"\nEnemyPos.z" << enemyPos.z
+			//	<< L"\nEnemyLife" << BrokenObjs::GetHP() << endl;
+			//scene->SetDebugString(wss.str());
+		//BrokenObjs::OnUpdate();
+		//}
 
-			BrokenObjs::OnUpdate();
+		m_stateMachine->Update(); //状態の更新	
 
+		diff = playerPos - enemyPos;
+		distance = diff.length();
+
+		if (distance < followRange && m_stateMachine->GetCurrentState() != ChasingState::Instance())
+		{
+			m_stateMachine->ChangeState(ChasingState::Instance());
 		}
+		else if(distance >= followRange && m_stateMachine->GetCurrentState() != WonderingState::Instance())
+		{
+			m_stateMachine->ChangeState(WonderingState::Instance());
+		}
+
+	}
+
+	void Enemy::Wondering()
+	{
+		float speed = 1.0f; //速度
+		timer += elapsedTime;
+
+		if (timer >= 0.0f)
+		{
+			SetValue();
+			dir = Vec3(rx, 0, rz).normalize();
+			timer = -1.0f + (float)rand() / RAND_MAX * -4.0f;
+		}
+
+		auto ground = GetStage()->GetSharedGameObject<Ground>(L"Ground");
+		Vec3 groundPos = ground->GetComponent<Transform>()->GetPosition();
+
+		if (enemyPos.x < groundPos.x)
+		{
+			enemyPos += dir * elapsedTime * speed;
+			m_transComp->SetPosition(enemyPos);
+		}
+		else if (enemyPos.z < groundPos.z)
+		{
+			enemyPos += dir * elapsedTime * speed;
+			m_transComp->SetPosition(enemyPos);
+		}
+	}
+
+	void Enemy::Chasing()
+	{
+		//Vec3 diff = playerPos - enemyPos;
+		//float distance = diff.length();
+
+		//float followRange = 5.0f; //追尾を開始する距離
+		//float followSpeed = 1.5f;//追尾時の速度
+
+		//if (distance < followRange)//一定距離以内
+		//{
+			dir = diff.normalize();
+
+			enemyPos += dir * elapsedTime * followSpeed;
+			m_transComp->SetPosition(enemyPos);
+
+		//}
+
+
 	}
 
 	void Enemy::SetValue()
--- GameSources/Enemy.h
+++ GameSources/Enemy.h
@@ -25,10 +25,21 @@ namespace basecross
 
 		bool isHit;
 		float timer;
+		float elapsedTime;
 
 		float rx;
 		float rz;
 		Vec3 dir;
+
+		Vec3 diff;
+		float distance;
+
+		float followRange = 5.0f; //追尾を開始する距離
+		float followSpeed = 1.5f;//追尾時の速度
+
+
+
+		unique_ptr<StateMachine<Enemy>> m_stateMachine;
 	public:
 		Enemy(const shared_ptr<Stage>& stage) :
 			BrokenObjs(stage),
@@ -39,6 +50,31 @@ namespace basecross
 		void OnCreate()override;
 		void OnUpdate()override;
 
+		void Wondering();
+		void Chasing();
+
+
+	};
+
+	class WonderingState : public ObjState<Enemy>
+	{
+		WonderingState() {}
+	public:
+		static shared_ptr<WonderingState> Instance();
+		virtual void Enter(const shared_ptr<Enemy>& Obj)override;
+		virtual void Execute(const shared_ptr<Enemy>& Obj)override;
+		virtual void Exit(const shared_ptr<Enemy>& Obj)override;
+
+	};
+
+	class ChasingState : public ObjState<Enemy>
+	{
+		ChasingState(){}
+	public:
+		static shared_ptr<ChasingState> Instance();
+		virtual void Enter(const shared_ptr<Enemy>& Obj)override;
+		virtual void Execute(const shared_ptr<Enemy>& Obj)override;
+		virtual void Exit(const shared_ptr<Enemy>& Obj)override;
 
 	};
 
