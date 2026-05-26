063bea51cbddc93a280a0d5eb56e10165470d966
Hikaru Katono <zeal24350106@fsgcl.jp>
2026-05-26 16:23:17 +0900

    Optionとポーズ用画像追加

---
+++ GameSources/Option.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "stdafx.h"
+#include "PNTDXModelDraw.h"
+
+namespace basecross
+{
+	enum class OptionItem
+	{
+		Return,
+		Restart,
+		StageSelect,
+		SoundTest,
+		Title,
+		Count //OptionItem数用
+	};
+	class Option : public GameObject
+	{
+		OptionItem m_optionItem;
+		int itemCount;
+		vector<shared_ptr<GameObject>> m_pauseUI;
+		Vec2 m_prevLStick;
+
+		void CreateUI();
+		bool m_isActive;
+		shared_ptr<GameObject> m_cursol; //カーソル用ポインタ
+		Vec3 cursolpositon; //カーソル位置更新用変数
+	public:
+		Option(const shared_ptr<Stage>& stage);
+		~Option();
+
+		void SetVisible(bool visible);
+
+		bool isActive() const 
+		{
+			return m_isActive; 
+		}
+
+		void OnCreate()override;
+		void OnUpdate()override;
+
+	};
+}
