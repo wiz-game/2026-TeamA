1d5b2929bebf79f3846326d6ca70ccc34e89bb47
Hikaru Katono <zeal24350106@fsgcl.jp>
2026-06-04 12:38:25 +0900

    クリア、ゲームオーバーのUI変更

--- GameSources/GameOverStage.cpp
+++ GameSources/GameOverStage.cpp
@@ -34,7 +34,7 @@ namespace basecross
 		auto& app = App::GetApp();
 		auto scene = app->GetScene<Scene>();
 		auto XAPtr = app->GetXAudio2Manager();
-		m_BGM = XAPtr->Start(L"GameOverBGM", 1, scene->m_BGMVolume);
+		m_BGM = XAPtr->Start(L"GameOverBGM", 0, scene->m_BGMVolume);
 
 	}
 
@@ -58,7 +58,7 @@ namespace basecross
 		auto& app = App::GetApp();
 		auto scene = app->GetScene<Scene>();
 
-		AddGameObject<Sprite>(L"TEX_GameOver", true, Vec3(1280, 840, 0) * 0.01f, Vec3(0, 0, 0));
+		AddGameObject<Sprite>(L"TEX_GameOver", true, Vec3(1024, 256, 0) * 0.01f, Vec3(0, 0, 0));
 
 	}
 
