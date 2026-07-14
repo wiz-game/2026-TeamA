74e8f5407d61996d378aaf98a60f266c3b5836e4
Hikaru Katono <zeal24350106@fsgcl.jp>
2026-07-14 14:39:17 +0900

    UI追加と変更

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -55,6 +55,7 @@ namespace basecross{
 		app->RegisterTexture(L"TEX_NUMBER", texPath + L"number.png");
 		app->RegisterTexture(L"TEX_NUMBER2", texPath + L"number2.png");
 		app->RegisterTexture(L"TEX_NUMBER2", texPath + L"number2.png");
+		app->RegisterTexture(L"TEX_APUSHUI", texPath + L"pushA.png");
 
 
 		// playerモデルの読み込み
